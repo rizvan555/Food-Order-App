1a8b78942dba1f50e60e6a733948919f80a98d35
RizvanKarimov <karimovrizvan84@gmail.com>
2023-05-20 21:05:43 +0200

    my first commit

---
+++ pages/home/index.jsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1 className=" text-red-600 font-dancing text-3xl">My Home Page</h1>
+    </div>
+  );
+};
+
+export default Index;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: ["14px", "20px"],
+        base: ["16px", "24px"],
+        lg: ["20px", "28px"],
+        xl: ["24px", "32px"],
+      },
+      colors: {
+        primary: "#ffbe33",
+        secondary: "#222831",
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
