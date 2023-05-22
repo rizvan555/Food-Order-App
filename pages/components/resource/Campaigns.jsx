53568e58a28350f0d0ba279480c35e05e8615e3a
RizvanKarimov <karimovrizvan84@gmail.com>
2023-05-23 00:18:34 +0200

    campane page without respponsive finished

--- pages/components/resource/Campaigns.jsx
+++ pages/components/resource/Campaigns.jsx
@@ -1,11 +1,27 @@
 import Image from "next/image";
 import o1 from "./Images/o1.jpg";
+import Title from "./Title";
+import { MdShoppingCart } from "react-icons/md";
 
 const CampaignItems = () => {
   return (
-    <div className="">
-      <div className=" border-4 border-primary rounded-full">
-        <Image src={o1} className=" w-32 h-32 rounded-full" />
+    <div className="flex items-center bg-secondary w-[30vw] p-3 gap-x-4">
+      <div className=" border-4 border-primary w-36 h-36 rounded-full p-1">
+        <Image
+          src={o1}
+          className=" w-32 h-32 rounded-full hover:scale-105 transition-all"
+        />
+      </div>
+      <div className=" flex flex-col text-white">
+        <Title className=" text-2xl">Tasty Thursdays</Title>
+        <div className=" text-white font-dancing">
+          <span className="text-[40px] ">20%</span>
+          <span className=" text-sm ml-1">Off</span>
+        </div>
+        <button className=" flex items-center mx-auto bg-primary rounded-3xl py-2 px-7 mt-3 gap-2">
+          <MdShoppingCart />
+          Order Now
+        </button>
       </div>
     </div>
   );
@@ -13,7 +29,7 @@ const CampaignItems = () => {
 
 const Campaigns = () => {
   return (
-    <div className=" flex container mx-auto py-20">
+    <div className=" flex justify-between container mx-auto py-20 w-[70vw] flex-wrap">
       <CampaignItems />
       <CampaignItems />
     </div>
