b64b2e1f8e23d7811c6b210f5f101f9d32e3d0c7
RizvanKarimov <karimovrizvan84@gmail.com>
2023-05-24 17:36:26 +0200

    input and map is ready

--- pages/components/Reservation.jsx
+++ pages/components/Reservation.jsx
@@ -2,28 +2,58 @@ import Input from "./forms/Input";
 import Title from "./resource/Title";
 
 const Reservation = () => {
+  const inputs = [
+    {
+      id: 1,
+      name: "fullName",
+      type: "text",
+      placeholder: "Your Full Name",
+    },
+    {
+      id: 2,
+      name: "phoneNumber",
+      type: "number",
+      placeholder: "Your Phone Name",
+    },
+    {
+      id: 3,
+      name: "fullEmail",
+      type: "email",
+      placeholder: "Your Email Address",
+    },
+    {
+      id: 4,
+      name: "persons",
+      type: "number",
+      placeholder: "How many persons",
+    },
+    {
+      id: 5,
+      name: "fullDate",
+      type: "datetime-local",
+      placeholder: "Your Date",
+    },
+  ];
+
   return (
     <div className="container mx-auto py-12">
-      <div className=" flex justify-between items-center gap-x-10 flex-wrap">
-        <div className="flex-1 flex-col">
-          <Title className=" text-[40px] mb-10">Book A Table</Title>
-          <div className=" flex flex-col gap-y-3 w-full">
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-            <Input />
+      <Title className=" text-[40px] mb-10">Book A Table</Title>
+      <div className=" flex justify-between gap-10 flex-wrap-reverse">
+        <div className="lg:flex-1 w-full">
+          <div className=" flex flex-col gap-y-3">
+            {inputs.map((input) => {
+              return <Input key={input.id} {...input} />;
+            })}
           </div>
           <button className="btn-primary mt-4">Book Now</button>
         </div>
-        <div className="md:flex-1 w-full">
+        <div className="lg:flex-1 ">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2592.3146840808126!2d7.773327341061286!3d49.47856245698245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479612664fb47f6f%3A0xd33b0d6811b6130c!2sErlenbacher%20Str.%2C%2067659%20Kaiserslautern!5e0!3m2!1sde!2sde!4v1684931221729!5m2!1sde!2sde"
-            width="600"
-            height="360"
+            className="md:w-[600px] w-[370px] md:h-[360px] h-[200px] "
             allowFullScreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </div>
--- pages/components/forms/Input.jsx
+++ pages/components/forms/Input.jsx
@@ -1,16 +1,22 @@
-const Input = () => {
+const Input = ({ type, placeholder }) => {
   return (
     <div>
       <div className=" w-full">
         <label className="relative block">
           <input
-            type="email"
-            className="h-14 w-full border border-primary outline-none px-4 peer cursor-text pt-2"
+            type={type}
+            className={`h-14 w-full border border-primary outline-none px-4 peer cursor-text pt-2 ${
+              type === "datetime-local" ? "pt-0" : ""
+            }`}
             required
           />
-          <span className="flex items-center absolute top-0 left-0 px-4 text-base h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h7 peer-valid:text-xs transitio-all">
-            Email
-          </span>
+          {type === "datetime-local" ? (
+            ""
+          ) : (
+            <span className="flex items-center absolute top-0 left-0 px-4 text-base h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h7 peer-valid:text-xs transitio-all">
+              {placeholder}
+            </span>
+          )}
         </label>
       </div>
     </div>
